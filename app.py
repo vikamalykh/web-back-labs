3cb9585da9eecf34e132575de6b47bd4b0226acb
Малых Виктория <vika.malykh.05@bk.ru>
2025-09-10 18:40:31 +0700

    Заголовки

--- app.py
+++ app.py
@@ -14,7 +14,10 @@ def web():
                 <h1>web-сервер на flask</h1>
                 <a href="/author">author</a>
             </body>
-        </html>"""
+        </html>""", 201, {
+            'X-Server': 'sample',
+            'Content-Type': 'text/plain; charset=utf-8'
+        }
 
 @app.route("/author")
 def author():
