91d73c2150ebb49a4d9f5a12730f4d2ec7308ef0
Vika Malykh <vika.malykh.05@bk.ru>
2025-09-14 13:37:47 +0700

    Стили

--- app.py
+++ app.py
@@ -37,13 +37,19 @@ def author():
 
 @app.route("/image")
 def image():
-    path = url_for("static", filename="oak.jpg")
-    return '''
+    css_path = url_for("static", filename="lab1.css")
+    image_path = url_for("static", filename="oak.jpg")
+    return f'''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" href="{css_path}">
+    </head>
     <body>
-        <h1>Дуб</h1>
-        <img src="''' + path + '''">
+        <div class="container">
+            <h1>Дуб</h1>
+            <img src="{image_path}">
+        </div>
     </body>
 </html>
 '''
