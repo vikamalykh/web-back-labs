ba8789ed325f7741f0fa2b1ae761c5f6907e98f2
Vika Malykh <vika.malykh.05@bk.ru>
2025-09-29 17:19:34 +0700

    Выражения

--- app.py
+++ app.py
@@ -467,3 +467,27 @@ def lab2():
 def filters():
     phrase = "О, <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
     return render_template('filter.html', phrase = phrase)
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    try:
+        divide_result = a / b
+    except ZeroDivisionError:
+        divide_result = 'Ошибка: деление на ноль'
+    
+    operations = {
+        'sum': a + b,
+        'subtract': a - b,
+        'multiply': a * b,
+        'divide': divide_result,
+        'power': a ** b
+    }
+    return render_template('calc.html', a=a, b=b, operations=operations)
+
+@app.route('/lab2/calc/')
+def calc_default():
+    return redirect('/lab2/calc/1/1')
+
+@app.route('/lab2/calc/<int:a>')
+def calc_single(a):
+    return redirect(f'/lab2/calc/{a}/1')
