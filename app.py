167229b3aa1391189d3048adf825b6dad9e5d1a9
Малых Виктория <vika.malykh.05@bk.ru>
2025-10-22 17:59:55 +0700

    Заготовка к 4-ой лабораторной

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from werkzeug.exceptions import HTTPException
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
+from lab4 import lab4
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
+app.register_blueprint(lab4)
 
 @app.route("/")
 @app.route("/start")
@@ -30,6 +32,7 @@ def index():
                 <li><a href="/lab1">Первая лабораторная</a></li>
                 <li><a href="/lab2/">Вторая лабораторная</a></li>
                 <li><a href="/lab3/">Третья лабораторная</a></li>
+                <li><a href="/lab4/">Четвёртая лабораторная</a></li>
             </ul>
         </nav>
         
