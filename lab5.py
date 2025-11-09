60cb3dc21ad96cb7bb1d1e2b46fa08478463df5c
Vika Malykh <vika.malykh.05@bk.ru>
2025-11-09 17:55:07 +0700

    Аутентификация

--- lab5.py
+++ lab5.py
@@ -7,7 +7,7 @@ lab5 = Blueprint('lab5', __name__)
 
 @lab5.route('/lab5/')
 def lab():
-    return render_template('lab5/lab5.html')
+    return render_template('lab5/lab5.html', login=session.get('login'))
 
 @lab5.route('/lab5/register', methods=['GET', 'POST'])
 def register():
@@ -43,3 +43,43 @@ def register():
     conn.close()
     
     return render_template('lab5/success.html', login=login)
+
+
+@lab5.route('/lab5/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab5/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+    
+    if not (login or password):
+        return render_template('lab5/login.html', error='Заполните поля')
+    
+    conn = psycopg2.connect(
+        host='127.0.0.1',
+        database='viktoriya_malykh_knowledge_base',
+        user='viktoriya_malykh_knowledge_base',
+        password='1234567'
+    )
+    cur = conn.cursor(cursor_factory=RealDictCursor)
+    
+    cur.execute(f"SELECT * FROM users WHERE login='{login}';")
+    user = cur.fetchone()
+    
+    if not user:
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    if not check_password_hash(user['password'], password):
+        cur.close()
+        conn.close()
+        return render_template('lab5/login.html', error='Логин и/или пароль неверны')
+    
+    session['login'] = login
+    
+    cur.close()
+    conn.close()
+    
+    return render_template('lab5/success_login.html', login=login)
