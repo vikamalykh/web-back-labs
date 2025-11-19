3d0df23e06df3f37810f3603713e82494b42147e
Малых Виктория <vika.malykh.05@bk.ru>
2025-11-19 16:49:22 +0700

    Заготовка к 6-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -18,6 +19,7 @@ app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
+app.register_blueprint(lab6)
 
 @app.route("/")
 @app.route("/start")
---
+++ lab6.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, url_for, request, render_template, make_response, redirect, session
+lab4 = Blueprint('lab6', __name__)
+
+@lab6.route('/lab6/')
+def main():
+    return render_template('lab6/lab6.html')
