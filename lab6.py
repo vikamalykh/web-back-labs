dab88c6d43bb51dcf59d762816c67c337e4215aa
Vika Malykh <vika.malykh.05@bk.ru>
2025-11-20 16:31:06 +0700

    Правки

--- lab6.py
+++ lab6.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, url_for, request, render_template, make_response, redirect, session
-lab4 = Blueprint('lab6', __name__)
+lab6 = Blueprint('lab6', __name__)
 
 @lab6.route('/lab6/')
 def main():
