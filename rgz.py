bbc6ef391279683b26706342e5593a81ba77d99e
Vika Malykh <vika.malykh.05@bk.ru>
2025-11-21 16:25:19 +0700

    Авторизация, регистрация и выход

--- rgz.py
+++ rgz.py
@@ -43,7 +43,9 @@ def main():
     furniture_items = cur.fetchall()
     db_close(conn, cur)
     
-    return render_template('rgz/main.html', furniture_items=furniture_items)
+    registered = request.args.get('registered')
+    
+    return render_template('rgz/main.html', furniture_items=furniture_items, registered=registered)
 
 
 @rgz.route('/rgz/register', methods=['GET', 'POST'])
@@ -74,7 +76,56 @@ def register():
         cur.execute("INSERT INTO rgz_users (login, password) VALUES (%s, %s);", (login, password_hash))
     else:
         cur.execute("INSERT INTO rgz_users (login, password) VALUES (?, ?);", (login, password_hash))
+
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT id FROM rgz_users WHERE login=%s;", (login,))
+    else:
+        cur.execute("SELECT id FROM rgz_users WHERE login=?;", (login,))
+    
+    user = cur.fetchone()
+    db_close(conn, cur)
+
+    session['login'] = login
+    session['user_id'] = user['id'] if current_app.config['DB_TYPE'] == 'postgres' else user[0]
+    
+    return redirect('/rgz/?registered=true')
+
+@rgz.route('/rgz/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('rgz/login.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+    
+    if not (login or password):
+        return render_template('rgz/login.html', error='Заполните поля')
+    
+    conn, cur = db_connect()
+    
+    if current_app.config['DB_TYPE'] == 'postgres':
+        cur.execute("SELECT * FROM rgz_users WHERE login=%s;", (login,))
+    else:
+        cur.execute("SELECT * FROM rgz_users WHERE login=?;", (login,))
+    
+    user = cur.fetchone()
+    
+    if not user:
+        db_close(conn, cur)
+        return render_template('rgz/login.html', error='Логин и/или пароль неверны')
+    
+    if not check_password_hash(user['password'], password):
+        db_close(conn, cur)
+        return render_template('rgz/login.html', error='Логин и/или пароль неверны')
+    
+    session['login'] = login
+    session['user_id'] = user['id'] if current_app.config['DB_TYPE'] == 'postgres' else user[0]
     
     db_close(conn, cur)
-    
-    return render_template('rgz/success.html', login=login)
+    return redirect('/rgz/')
+
+@rgz.route('/rgz/logout')
+def logout():
+    session.pop('login', None)
+    session.pop('user_id', None)
+    return redirect('/rgz/')
