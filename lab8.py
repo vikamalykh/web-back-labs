48aca6a7183e307762154ccf04e3eb073e54d041
Vika Malykh <vika.malykh.05@bk.ru>
2025-12-10 16:06:52 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -4,35 +4,39 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 import psycopg2
 from psycopg2.extras import RealDictCursor
-import os
-from os import path
+from db import db
+from db.models import users, articles
 
 lab8 = Blueprint('lab8', __name__)  
-
-def db_connect():
-    if current_app.config['DB_TYPE'] == 'postgres':
-        conn = psycopg2.connect(
-            host='127.0.0.1',
-            database='viktoriya_malykh_knowledge_base',
-            user='viktoriya_malykh_knowledge_base',
-            password='1234567'
-        )
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-    else:
-        dir_path = path.dirname(path.realpath(__file__))
-        db_path = path.join(dir_path, "database.db")
-        conn = sqlite3.connect(db_path)
-        conn.row_factory = sqlite3.Row
-        cur = conn.cursor()
-    
-    return conn, cur
-
-def db_close(conn, cur):
-    conn.commit()
-    cur.close()
-    conn.close()
 
 @lab8.route('/lab8/')
 def main():
     login = session.get('login')
     return render_template('lab8/lab8.html', login=login)
+
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/register.html', error='Логин не может быть пустым')
+
+    if not password_form:
+        return render_template('lab8/register.html', error='Пароль не может быть пустым')
+    
+    login_exists = users.query.filter_by(login=login_form).first()
+    
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login=login_form, password=password_hash)
+    
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return redirect('/lab8/')
