95e447802eb7bd6d2f116d45cffa88e7864eebdd
Vika Malykh <vika.malykh.05@bk.ru>
2025-12-10 18:09:41 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -6,6 +6,7 @@ import psycopg2
 from psycopg2.extras import RealDictCursor
 from db import db
 from db.models import users, articles
+from flask_login import login_user, logout_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)  
 
@@ -40,3 +41,32 @@ def register():
     db.session.commit()
     
     return redirect('/lab8/')
+
+@lab8.route('/lab8/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form:
+        return render_template('lab8/login.html', error='Введите логин')
+    if not password_form:
+        return render_template('lab8/login.html', error='Введите пароль')
+
+    user = users.query.filter_by(login=login_form).first()
+    
+    if not user:
+        return render_template('lab8/login.html', error='Неверный логин или пароль')
+    
+    if check_password_hash(user.password, password_form):
+        login_user(user, remember=False)
+        return redirect('/lab8/')
+    else:
+        return render_template('lab8/login.html', error='Неверный логин или пароль')
+    
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "список статей"
