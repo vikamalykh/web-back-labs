2aadf3dbd83662a87ffdf1a0924e68cb5afe5c8f
Vika Malykh <vika.malykh.05@bk.ru>
2025-12-11 15:56:13 +0700

    Заготовка к 9ой лабораторной

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 from rgz import rgz
 
 app = Flask(__name__)
@@ -53,6 +54,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 app.register_blueprint(rgz)
 
 @app.route("/")
@@ -80,6 +82,7 @@ def index():
                 <li><a href="/lab6/">Шестая лабораторная</a></li> 
                 <li><a href="/lab7/">Седьмая лабораторная</a></li>
                 <li><a href="/lab8/">Восьмая лабораторная</a></li>
+                <li><a href="/lab9/">Девятая лабораторная</a></li>
                 <li><a href="/rgz/">Расчётно-графическая работа</a></li>
             </ul>
         </nav>
